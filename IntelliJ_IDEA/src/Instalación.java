b1a341a79405bdf486ea60c5b5b056c7c1fe55ad
Ricardo Hernández <rhernandezb5@miumg.edu.gt>
2022-07-20 22:49:35 -0600

    Tarea 2 Clase 2 Herramienta de versionamiento de codigo

---
+++ IntelliJ_IDEA/src/Instalación.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Instalación {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Ingresa tu nombre");
+
+        String nombre = entrada.nextLine();
+
+        System.out.println("\nBienvenido a Progra 2 2022 " + nombre);
+
+
+
+    }
+    
+    
+    
+    
+    
+
+    
+    
+}
